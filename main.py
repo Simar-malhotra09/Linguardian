5646fdae2e489e5a1966fd14ba0544e2f7c31bed
Simar Malhotra <malhotrasimar009@gmail.com>
2024-11-19 09:33:32 -0500

    maintainance change

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def main():
     # Set up argument parsing
     parser = argparse.ArgumentParser(description="Extract OCR text from a PDF and blur English words.")
     parser.add_argument('--file', type=str, required=True, help='Path to the input PDF file')
-    parser.add_argument('--output', type=str, required=True, help='Path to save the OCR output text file')
+    parser.add_argument('--output', type=str, required=True, help='Path to save the blurred images ')
 
     args = parser.parse_args()
 
-    # Create an instance of PDFTextBlurrer and process the PDF
+    
     blurrer = Linguardian(args.file, args.output)
     blurrer.process_pdf()
 
